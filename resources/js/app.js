380ed69570bf75bf9e7d2f4e0be24d9b66425634
Tiph06 <you@example.com>
2025-06-11 22:35:39 +0200

    ajout leaft

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,14 @@ import './bootstrap';
 
 import Alpine from 'alpinejs';
 
+import Chart from 'chart.js/auto';
+import L from 'leaflet';
+
+
 window.Alpine = Alpine;
 
 Alpine.start();
+
+
+window.Chart = Chart;
+window.L = L;
